d16dc671476df11673505dd845ce241be101d21d
nickgu <nickgu@localhost.localdomain>
2015-02-21 20:47:14 -0800

    restful api sample

---
+++ Restful API Sample/RestDemo1/RestDemo1/Controllers/ContactController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestDemo1.Models;
+using RestDemo1.Services;
+
+namespace RestDemo1.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepositry contactRepositry;
+
+        public ContactController()
+        {
+            this.contactRepositry = new ContactRepositry();
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepositry.GetAllContacts();
+        }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepositry.SaveContact(contact);
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+            
+            return response;
+        }
+    }
+    
+}
---
+++ Restful API Sample/RestDemo1/RestDemo1/Services/ContactRepositry.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using RestDemo1.Models;
+
+namespace RestDemo1.Services
+{
+    public class ContactRepositry
+    {
+        private const string CacheKey = "ContactStore";
+
+        public ContactRepositry()
+        {
+            var ctx = HttpContext.Current;
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var contacts = new Contact[]{
+                        new Contact
+                        {
+                             Id = 1,
+                             Name = "Glenn Block"
+                        },
+                        new Contact
+                        {
+                             Id = 2,
+                             Name = "Dan Roth"
+                        }
+                    };
+
+                    ctx.Cache[CacheKey] = contacts;
+                }
+            }
+        }
+
+        public Contact[] GetAllContacts()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null && ctx.Cache[CacheKey] != null)
+            {
+                return (Contact[])ctx.Cache[CacheKey];
+            }
+
+            return new Contact[]{
+                new Contact{
+                    Id = 0,
+                    Name = "Placeholder"
+                }
+            };
+        }
+
+        public bool SaveContact(Contact contact)
+        {
+            var ctx = HttpContext.Current;
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Contact[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(contact);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+                    //save to backend db
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
